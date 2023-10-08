e84ecebc746779cc5af611ee70e66e898e97b02f
Gabriel Azorín <ga.azorin@gmail.com>
2023-10-07 22:43:38 -0300

    Se terminó "agregar_sorteo"

--- main.py
+++ main.py
@@ -4,47 +4,42 @@ import sqlite3
 # Configuración de la base de datos
 DB_NAME = 'loto.db'
 
-
-
 def agregar_sorteo(sorteo_dict):
     """Agrega un sorteo a la base de datos."""
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     
-    sorteo_id = sorteo_dict['sorteo_id']
-    day = sorteo_dict['day']
-    month = sorteo_dict['month']
-    year = sorteo_dict['year']
-    weekday = sorteo_dict['weekday']
-    n1_loto = sorteo_dict['n1_loto']
-    n2_loto = sorteo_dict['n2_loto']
-    n3_loto = sorteo_dict['n3_loto']
-    n4_loto = sorteo_dict['n4_loto']
-    n5_loto = sorteo_dict['n5_loto']
-    n6_loto = sorteo_dict['n6_loto']
-    comodin = sorteo_dict['comodin']
-    money_per_winner_loto = sorteo_dict['money_per_winner_loto']
-    amonut_of_winners_loto = sorteo_dict['amonut_of_winners_loto']
-    money_per_winner_super_quina = sorteo_dict['money_per_winner_super_quina']
-    amonut_of_winners_super_quina = sorteo_dict['amonut_of_winners_super_quina']
-    money_per_winner_quina = sorteo_dict['money_per_winner_quina']
-    amonut_of_winners_quina = sorteo_dict['amonut_of_winners_quina']
-
     # Verificar si el sorteo ya existe
-    cursor.execute('SELECT * FROM sorteos WHERE sorteo_id=?', (sorteo_id,))
+    cursor.execute('SELECT * FROM sorteos WHERE sorteo_id=?', (sorteo_dict['sorteo_id'],))
     if cursor.fetchone():
-        print(f"El sorteo {sorteo_id} ya existe.")
+        print(f"El sorteo {sorteo_dict['sorteo_id']} ya existe.")
         return False
     
     # Agregar el sorteo
-    cursor.execute('INSERT INTO sorteos (sorteo_id, day, month, year, weekday, n1_loto, n2_loto, n3_loto, n4_loto, n5_loto, n6_loto comodin,) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
-                   (sorteo_id, day, month, year, weekday, n1_loto, n2_loto, n3_loto, n4_loto, n5_loto, n6_loto,
-                    comodin, money_per_winner_loto, amonut_of_winners_loto, money_per_winner_super_quina, amonut_of_winners_super_quina,
-                    money_per_winner_quina, amonut_of_winners_quina))
+    campos = ['sorteo_id', 'day', 'month', 'year', 'week_day', 'n1_loto', 'n2_loto', 'n3_loto', 'n4_loto', 'n5_loto', 'n6_loto',
+              'comodin', 'money_per_winner_loto', 'amount_of_winners_loto', 'money_per_winner_super_quina', 'amount_of_winners_super_quina',
+              'money_per_winner_quina', 'amount_of_winners_quina', 'money_per_winner_super_cuaterna', 'amount_of_winners_super_cuaterna',
+              'money_per_winner_cuaterna', 'amount_of_winners_cuaterna', 'money_per_winner_super_terna', 'amount_of_winners_super_terna',
+              'money_per_winner_terna', 'amount_of_winners_terna', 'money_per_winner_super_dupla', 'amount_of_winners_super_dupla',
+              'n1_recargado', 'n2_recargado', 'n3_recargado', 'n4_recargado', 'n5_recargado', 'n6_recargado', 'money_per_winner_recargado',
+              'amount_of_winners_recargado', 'n1_revancha', 'n2_revancha', 'n3_revancha', 'n4_revancha', 'n5_revancha', 'n6_revancha',
+              'money_per_winner_revancha', 'amount_of_winners_revancha', 'n1_desquite', 'n2_desquite', 'n3_desquite', 'n4_desquite',
+              'n5_desquite', 'n6_desquite', 'money_per_winner_desquite', 'amount_of_winners_desquite']
+    valores = [sorteo_dict[campo] for campo in campos]
     
-    conn.commit()
-    conn.close()
+    consulta = f"INSERT INTO sorteos ({', '.join(campos)}) VALUES ({', '.join(['?']*len(campos))})"
+    
+    try:
+        cursor.execute(consulta, valores)
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error al agregar sorteo: {e}")
+        return False
+    finally:
+        conn.close()
+
     return True
+
 
 def calcular_sorteos_faltantes(primer_sorteo, primer_numero_sorteo):
     conn = sqlite3.connect(DB_NAME)
@@ -81,20 +76,20 @@ primer_sorteo = datetime.datetime(2016, 1, 3, 21, 0)
 primer_numero_sorteo = 3803
 
 # Mostrar mensaje
-#mostrar_mensaje_bienvenida(primer_sorteo, primer_numero_sorteo)
+mostrar_mensaje_bienvenida(primer_sorteo, primer_numero_sorteo)
 
 sorteo = {
 	'sorteo_id': 3803,
 	'day': 3,
 	'month': 1,
 	'year': 2016,
-    'weekday': 'domingo',
-	'N1_loto': 7,
-	'N2_loto': 14,
-	'N3_loto': 16,
-	'N4_loto': 19,
-	'N5_loto': 24,
-	'N6_loto': 35,
+    'week_day': 'domingo',
+	'n1_loto': 7,
+	'n2_loto': 14,
+	'n3_loto': 16,
+	'n4_loto': 19,
+	'n5_loto': 24,
+	'n6_loto': 35,
 	'comodin': 5,
 	'money_per_winner_loto': 0,
 	'amount_of_winners_loto': 0,
@@ -112,28 +107,28 @@ sorteo = {
 	'amount_of_winners_terna': 10517,
 	'money_per_winner_super_dupla': 1000,
 	'amount_of_winners_super_dupla': 9919,
-	'N1_recargado': 2,
-	'N2_recargado': 17,
-	'N3_recargado': 18,
-	'N4_recargado': 28,
-	'N5_recargado': 31,
-	'N6_recargado': 41,
+	'n1_recargado': 2,
+	'n2_recargado': 17,
+	'n3_recargado': 18,
+	'n4_recargado': 28,
+	'n5_recargado': 31,
+	'n6_recargado': 41,
 	'money_per_winner_recargado': 0,
 	'amount_of_winners_recargado': 0,
-	'N1_revancha': 1,
-	'N2_revancha': 8,
-	'N3_revancha': 9,
-	'N4_revancha': 27,
-	'N5_revancha': 30,
-	'N6_revancha': 38,
+	'n1_revancha': 1,
+	'n2_revancha': 8,
+	'n3_revancha': 9,
+	'n4_revancha': 27,
+	'n5_revancha': 30,
+	'n6_revancha': 38,
 	'money_per_winner_revancha': 0,
 	'amount_of_winners_revancha': 0,
-	'N1_desquite': 2,
-	'N2_desquite': 17,
-	'N3_desquite': 18,
-	'N4_desquite': 28,
-	'N5_desquite': 31,
-	'N6_desquite': 41,
+	'n1_desquite': 2,
+	'n2_desquite': 17,
+	'n3_desquite': 18,
+	'n4_desquite': 28,
+	'n5_desquite': 31,
+	'n6_desquite': 41,
 	'money_per_winner_desquite': 0,
 	'amount_of_winners_desquite': 0,
 }
